58b21cacf5b5aa7dd96ebdcceef49260b00aac80
lam-hurman21 <168532746+lam-hurman21@users.noreply.github.com>
2026-06-26 15:38:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-
-
 const video = document.getElementById('wildlife-video');
 const videoBtn = document.getElementById('video-btn');
-const videoContainer = document.querySelector('.video-container');
+const videoWrapper = document.getElementById('video-wrapper');
 
-// Synchronize button text if the user uses native video controls instead
+videoBtn.addEventListener('click', () => {
+    if (video.paused) {
+        videoWrapper.style.display = 'block';
+        video.play();
+        videoBtn.textContent = 'Hide Video';
+    } else {
+        
+        video.pause();
+        videoWrapper.style.display = 'none';
+        videoBtn.textContent = 'Play Video';
+    }
+});
+
 video.addEventListener('play', () => {
     videoBtn.textContent = 'Hide Video';
 });
 
 video.addEventListener('pause', () => {
     videoBtn.textContent = 'Play Video';
-});
-
-videoBtn.addEventListener('click', () => {
-    if (video.paused) {
-        
-        videoContainer.style.display = 'block';
-        video.play();
-        videoBtn.textContent = 'Hide Video';
-    } else {
-       
-        video.pause();
-        videoContainer.style.display = 'none';
-        videoBtn.textContent = 'Play Video';
-    }
 });
